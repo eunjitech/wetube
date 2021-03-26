b5e85cfa689db6ab0c186bdf45d65b5dbe5f0e99
eunji Lee <eunjitech@gmail.com>
2021-03-26 18:35:25 +0900

    Express.js 완료

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,7 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import {videos} from "../db";
+import routes from "../routes";
+
+export const home = (req, res) => res.render("home", { pageTitle: "Home", videos });
 
 export const search = (req, res) =>{
   const{
@@ -7,10 +10,21 @@ export const search = (req, res) =>{
     }
   } = req;
   //const searchingBy = req.query.term
-  res.render("search", { pageTitle: "Search", searchingBy});
+  res.render("search", { pageTitle: "Search", searchingBy, videos});
 }
 
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const{
+    body: {
+      file, title, description
+    }
+  } = req;
+  
+  //업로드와 비디오 저장
+  res.redirect(routes.videoDetail(32453456));
+};
 
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 
--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,10 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = 'Wetube';
     res.locals.routes = routes;
+    //가짜 user 데이터 생성(테스트용)
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1
+    };
     next();
 };
