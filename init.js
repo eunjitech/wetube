46e317b448e830c75db0812c910b6014a23d0367
eunji Lee <eunjitech@gmail.com>
2021-03-29 17:19:13 +0900

    ADD dotenv

--- init.js
+++ init.js
@@ -1,8 +1,11 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`💜Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
   
