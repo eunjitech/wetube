211117de04b08936a06d35acba566f99cf5a063f
eunji Lee <eunjitech@gmail.com>
2021-04-01 14:45:22 +0900

    ADD Search Function

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,29 +1,29 @@
-import express from "express";
-import routes from "../routes";
+import express from 'express';
+import routes from '../routes';
 import {
-    getUpload,
-    postUpload,
-    videoDetail,
-    getEditVideo,
-    postEditVideo,
-    deleteVideo
-  } from "../controllers/videoController";
-import {uploadVideo} from "../middlewares";
+  getUpload,
+  postUpload,
+  videoDetail,
+  getEditVideo,
+  postEditVideo,
+  deleteVideo,
+} from '../controllers/videoController';
+import { uploadVideo } from '../middlewares';
 
 const videoRouter = express.Router();
 
-//uploac
+// uploac
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
 
-//video detail
+// video detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
-//edit video
+// edit video
 videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.post(routes.editVideo(), postEditVideo);
 
-//delete
+// delete
 videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
