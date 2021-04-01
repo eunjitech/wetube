4432dbfcc3bbabfeeb6098bb9c7931cb286f2f1d
eunji Lee <eunjitech@gmail.com>
2021-04-01 18:06:47 +0900

    ADD Webpack

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import {localsMiddleware} from "./middlewares";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -11,25 +11,24 @@ import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-//middleware
+// middleware
 app.use(
-    helmet({
-        contentSecurityPolicy: false
-    })
-);  
-app.set("view engine", "pug"); //뷰엔진을 퍼그로 바꿈
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
+app.set("view engine", "pug"); // 뷰엔진을 퍼그로 바꿈
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-//middleware
+// middleware
 app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 app.use(localsMiddleware);
 
-//router
+// router
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
