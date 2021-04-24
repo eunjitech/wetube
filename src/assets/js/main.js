c32d7bacf7b08a96539dd8879d916c732f4ea744
eunji Lee <eunjitech@gmail.com>
2021-04-24 13:28:37 +0900

    ADD Loading message

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,3 +2,7 @@ import "../scss/styles.scss";
 import "./videoPlayer";
 import "./videoRecorder";
 import "./addComment";
+
+window.addEventListener("load", function load() {
+  document.querySelector("body").innerHTML = `<h1>로딩중입니다..🧐</h1>`;
+});
